ea7ff3e983986ff24befe07b7da04e504c85315a
Andrew James <andrew@semaja2.net>
2016-03-27 16:53:36 +1030

    Initial Commit

---
+++ CalendarExtract/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalendarExtract
+//
+//  Created by Andrew James on 27/03/2016.
+//  Copyright © 2016 Andrew James. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
