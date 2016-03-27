3ba2c801db92510d789bf84e2257ca00c5c1f175
Andrew James <andrew@semaja2.net>
2016-03-28 00:14:22 +1030

    Code Cleanup

--- CalendarExtract/main.swift
+++ CalendarExtract/main.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 import EventKit
-//import CommandLine
 
 let cli = CommandLine()
 
@@ -53,16 +52,6 @@ if (calendersToSearchOption.wasSet && calendersToSearchOption.value! != "") {
     calendarsToSearch = calendersToSearchOption.value!.componentsSeparatedByString(",")
 }
 
-if Process.arguments.count < 2 {
-    // Expecting a string but didn't receive it
-}
-else {
-    //let args = Pro
-    let arg = Process.arguments[1]
-    
-    
-    print(arg)
-}
 
 
 extension NSDate {
@@ -87,7 +76,6 @@ func checkCalendarAuthorizationStatus() {
     
     switch (status) {
     case EKAuthorizationStatus.NotDetermined:
-        // This happens on first-run
         print("Requesting authorization to calendars")
         requestAccessToCalendar()
     case EKAuthorizationStatus.Authorized:
@@ -96,6 +84,7 @@ func checkCalendarAuthorizationStatus() {
         //refreshTableView()
     case EKAuthorizationStatus.Restricted, EKAuthorizationStatus.Denied:
         print("Access has been restriced, please authorise this application to continue")
+        print("Access has been restricted, please authorize this application to continue")
         exit(1);
         // We need to help them give us permission
         //needPermissionView.fadeIn()
@@ -109,7 +98,6 @@ func requestAccessToCalendar() {
         if accessGranted == true {
             dispatch_async(dispatch_get_main_queue(), {
                 loadCalendars()
-                //self.refreshTableView()
             })
         } else {
             dispatch_async(dispatch_get_main_queue(), {
@@ -150,6 +138,8 @@ func loadCalendars() {
         outputString = exportToICSFormat(arrayOfEvents)
     case "text":
         outputString = exportToTextFormat(arrayOfEvents)
+    case "json":
+        outputString = exportToJSONFormat(arrayOfEvents)
     default:
         outputString = exportToTextFormat(arrayOfEvents)
     }
@@ -203,7 +193,6 @@ func exportToICSFormat(eventsToExport: NSArray) -> String {
             
             //get startdate and add 1 day for the end date.
             let addDay = NSDate(timeIntervalSinceNow: +86400)
-            //NSDate *addDay = [self.startDate dateByAddingTimeInterval:86400];
             let allDayEnd = format1.stringFromDate(addDay)
             
             iCalString = iCalString + "\r\nDTEND;VALUE=DATE:\(allDayEnd)"
@@ -213,26 +202,20 @@ func exportToICSFormat(eventsToExport: NSArray) -> String {
             
         else {
             
-            //if (((event.startDate) != nil) && ((event.endDate) != nil)) {
-                iCalString = iCalString + "\r\nDTSTART;TZID=Australia/Adelaide:"
-                
-                let format2 = NSDateFormatter()
-                format2.dateFormat = "yyyyMMdd'T'HHmmss"
-                
-                let dateAsString = format2.stringFromDate(event.startDate)
-                iCalString = iCalString + dateAsString
-                //end date
-                
-                iCalString = iCalString + "\r\nDTEND;TZID=Australia/Adelaide:"
-                
-                let dateAsString1 = format2.stringFromDate(event.endDate)
-                
-                iCalString = iCalString + dateAsString1
-                
-            //}
-            //else {
-            //    print("****Error****Missing one of needed values: startDate or endDate")
-            //}
+            iCalString = iCalString + "\r\nDTSTART;TZID=Australia/Adelaide:"
+            
+            let format2 = NSDateFormatter()
+            format2.dateFormat = "yyyyMMdd'T'HHmmss"
+            
+            let dateAsString = format2.stringFromDate(event.startDate)
+            iCalString = iCalString + dateAsString
+            //end date
+            
+            iCalString = iCalString + "\r\nDTEND;TZID=Australia/Adelaide:"
+            
+            let dateAsString1 = format2.stringFromDate(event.endDate)
+            
+            iCalString = iCalString + dateAsString1
         }
         
         iCalString = iCalString + "\r\nDTSTAMP:"    //date the event was created
@@ -252,9 +235,7 @@ func exportToICSFormat(eventsToExport: NSArray) -> String {
             iCalString = iCalString + dateAsString2
             
         }
-        //[format3 release];
-        //UID is generated randomly
-        //NSString *a = [self genRandStringLength];
+
         iCalString = iCalString + "\r\nUID:\(event.eventIdentifier)"
         
         
@@ -280,9 +261,8 @@ func exportToICSFormat(eventsToExport: NSArray) -> String {
         
         
         //Event Title
-        //if (event.title != nil) {
-            iCalString = iCalString + "\r\nSUMMARY:\(event.title)"
-        //}
+        iCalString = iCalString + "\r\nSUMMARY:\(event.title)"
+        
         //Notes
         if (event.notes != nil) {
             iCalString = iCalString + "\r\nDESCRIPTION:\(event.notes)"
